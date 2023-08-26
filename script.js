5ed49bba0ca4b8f153fe53c0c2ee86b6c5f78492
Vidhi <srivastavavidhi29@gmail.com>
2023-08-26 10:12:32 +0530

    Learning XML Http request

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const request = new XMLHttpRequest();
 const button  = document.querySelector(".button")
 const info = document.querySelector(".info")
+
 request.addEventListener('readystatechange', () => {
     //console.log("request: ", request, "readyState: ", request.readyState);
-    console.log(request.responseText)
+    console.log(JSON.parse(request.responseText))
     if(request.readyState === 4){
         button.addEventListener('click', (e) => {
             e.preventDefault()
-            const html = `<span>${request.responseText}<span>`
+            const html = `<span>${JSON.parse(request.responseText)}<span>`
             info.innerHTML = html
         })
     }
