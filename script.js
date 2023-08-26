977bd068bd33e6e866f9a6161dffd17e03f5a637
Vidhi <srivastavavidhi29@gmail.com>
2023-08-26 10:34:23 +0530

    Learning Fetch and promises

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
-const request = new XMLHttpRequest();
+// const request = new XMLHttpRequest();
 const button  = document.querySelector(".button")
 const info = document.querySelector(".info")
 
-request.addEventListener('readystatechange', () => {
-    //console.log("request: ", request, "readyState: ", request.readyState);
-    console.log(JSON.parse(request.responseText))
-    if(request.readyState === 4){
-        button.addEventListener('click', (e) => {
-            e.preventDefault()
-            const html = `<span>${JSON.parse(request.responseText)}<span>`
-            info.innerHTML = html
-        })
-    }
-})
+// request.addEventListener('readystatechange', () => {
+//     //console.log("request: ", request, "readyState: ", request.readyState);
+//     console.log(JSON.parse(request.responseText))
+//     if(request.readyState === 4){
+//         button.addEventListener('click', (e) => {
+//             e.preventDefault()
+//             const html = `<span>${JSON.parse(request.responseText)}<span>`
+//             info.innerHTML = html
+//         })
+//     }
+// })
 
-request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
-request.send()
+// request.open('GET', 'https://jsonplaceholder.typicode.com/todos/')
+// request.send()
+
+
+fetch("vidhi.json").then((response) => {
+    console.log('resolved', response);
+    return response.json();
+}).then(data => {
+    button.addEventListener('click', (e) => {
+        e.preventDefault()
+        const html = `<span>${data}</span>`
+        info.innerHTML = html
+    })
+    console.log(data)
+}).catch(err => {
+    console.log('rejected', err)
+})
